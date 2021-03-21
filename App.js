a4e6cdb8d5fc9e6df5f445e1d42bda322e44dd23
Faradawn Yang <faradawn@uchicago.edu>
2021-03-21 13:34:43 +0800

    navigate param counter

--- App.js
+++ App.js
@@ -1,26 +1,44 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
 
-function HomeScreen({navigation}) {
+function HomeScreen({route, navigation, navigation:{ setParams }}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen 2</Text>
-      <Button title='go to detail' onPress={() => navigation.navigate('Detail')}></Button>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'cornSilk' }}>
+      <Text>Home Screen</Text>
+      <Text>Home Counter: {route.params.count} </Text>
+      <Button title='to detail' onPress={() => navigation.navigate('Detail', {
+        id: 10,
+        item: 'item1',
+      })}/>
+      <Button title='setParams' onPress={() => setParams({
+        count: route.params.count + 1
+      }) }></Button>
     </View>
   );
 }
 
-function Detail({navigation}) {
+function Detail({route, navigation}) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text> Detail Screen</Text>
-      <Button title='push title' onPress={() => navigation.goBack() }></Button>
+      <Text> id: {route.params.id} </Text>
+      <Text> item: {route.params.item} </Text> 
+      <Button title='go Home' onPress={() => navigation.popToTop('home', {count: 10})} ></Button>
       
       </View>
+  )
+}
+
+function firstScreen({navigation}) {
+  return (
+    <View>
+      <Text> First </Text>
+      <Button title='go home' onPress={() => navigation.navigate('Home')}></Button>
+    </View>
   )
 }
 
@@ -29,13 +47,25 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} initialParams={{count: 0}}/>
+        <Stack.Screen name="Top" component={firstScreen} />
         <Stack.Screen name="Detail" component={Detail} options={{title: 'Detail'}}/>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        
+        
         
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'cornsilk',
+    padding: 20,
+  }
+})
 
 export default App;
