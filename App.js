26ca5804b69bd13b4d2deacb7baff54b43c436ad
Faradawn Yang <faradawn@uchicago.edu>
2021-03-23 14:28:47 +0800

    added map marker

--- App.js
+++ App.js
@@ -1,30 +1,55 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
 import { TextInput } from 'react-native-gesture-handler';
 import Settings from './screens/Settings';
 import styles from './assets/styles';
+import MapView, { Marker } from 'react-native-maps';
+import { concat } from 'react-native-reanimated';
+
 
 function HomeScreen({route, navigation, navigation:{ setParams }}) {
-  const [search, setSearch] = React.useState('')
+  let regionInit = {
+    latitude: 40.0055728,
+    longitude: 116.347646,
+    latitudeDelta: 0.0059107,
+    longitudeDelta: 0.0062788, 
+  }
+  let markerInit = {
+    latitude: 40.0059372,
+    longitude: 116.3474919,
+  }
+  const [region, setRegion] = React.useState(regionInit)
+  const [x, setX] = React.useState(markerInit)
+
   return (
     <View style={styles.container}>
+      <View>
+      <MapView style={styles.map}
+        initialRegion={region}
+        
+        onRegionChange={setRegion}
+        region={region}
+      >
+         <Marker draggable
+          coordinate={x}
+          onDragEnd={(e) => setX(e.nativeEvent.coordinate)}
+          title='在这里！'
+          description={'经度'.concat((Math.round(x.latitude * 100)/100).toString()).concat(', 纬度'.concat((Math.round(x.longitude*100)/100).toString()))}
+         />
+      </MapView>
+      <Button title='重置' onPress={() => (setRegion(regionInit), setX(markerInit))}></Button>
+      </View>
       
-      <Text> {search} </Text>
+      <Text>x {x.latitude} , {x.longitude}</Text>
+      <Text>{region.latitudeDelta} , {region.longitudeDelta}</Text>
 
-      <Button title='go Settings' onPress={() => navigation.navigate('Settings')}/>
-
-      <TextInput
-        style={styles.search}
-        placeholder='输入目的地'
-        value={search}
-        onChangeText={setSearch}/>
-      
-
+      <Button title='Settings' onPress={() => navigation.navigate('Settings')}/>
     </View>
+    
   );
 }
 
--- assets/styles.js
+++ assets/styles.js
@@ -1,18 +1,22 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
 
 export default StyleSheet.create({
   container: {
-    backgroundColor: 'cornsilk',
+    backgroundColor: 'white',
     alignItems: 'center',
     flex: 1,
   },
   search: {
-    position: 'relative',
-    top: 650,
     borderWidth: 1,
     borderRadius: 5,
     borderColor: 'lightgrey',
     paddingHorizontal: 130,
     paddingVertical: 5,
-  }
+    zIndex: 2,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height - 400,
+    marginBottom: 10,
+  },
 })
